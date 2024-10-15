0df1a0e2cde0cfd49099489050b6ab16ffe5e057
Aman <aman.nasir231@gmail.com>
2024-10-16 01:02:31 +0500

    add validation in register api

--- server/model/User.js
+++ server/model/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema(
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -12,6 +12,11 @@ const UserSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    role: {
+      type: String,
+      enum: ["admin", "customer"],
+      default: "customer",
+    },
     password: {
       type: String,
       required: true,
@@ -20,4 +25,14 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
+module.exports = mongoose.model("User", userSchema);
--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
 const router = express.Router();
+const {
+  loginValidationSchemma,
+  signupValidationSchemma,
+} = require("../validation/authValidation");
 
-router.post("/login", (req, res) => {
-  res.send("Hello world");
+router.post("/login", (req, res) => {});
+
+router.post("/register", (req, res) => {
+  const { error } = signupValidationSchemma.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  console.log(req.body);
 });
-
-router.post("/register", (req, res) => {});
 
 module.exports = router;
